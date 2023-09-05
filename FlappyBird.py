29a252e25cc75252a620c2f99c4b6829145c140b
GuttoRegadas <guttoregadas@hotmail.com>
2023-09-05 04:25:57 -0300

    Modify

---
+++ FlappyBird.py
@@ -0,0 +1,11 @@
+import pygame
+import os
+import random
+
+TELA_LARGURA = 500
+TELA_ALTURA = 800
+
+IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
+IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
+IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
+IMAGENS_PASSARO = 
