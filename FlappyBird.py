e0e50b914920e09662ddbb4303c44937b4c8b122
GuttoRegadas <guttoregadas@hotmail.com>
2023-09-27 05:24:32 -0300

    modify002

--- FlappyBird.py
+++ FlappyBird.py
@@ -8,4 +8,4 @@ TELA_ALTURA = 800
 IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
 IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
 IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
-IMAGENS_PASSARO = 
+#IMAGENS_PASSARO =
